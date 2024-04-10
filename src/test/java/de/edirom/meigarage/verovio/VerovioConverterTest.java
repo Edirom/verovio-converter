338a57fea529bcb5509565816d90d2dc229ab77a
Anne Ferger <anne.ferger@uni-paderborn.de>
2024-04-10 13:53:39 +0200

    add file compare to unit test refs Edirom/MEIGarage#13

--- src/test/java/de/edirom/meigarage/verovio/VerovioConverterTest.java
+++ src/test/java/de/edirom/meigarage/verovio/VerovioConverterTest.java
@@ -1,10 +1,13 @@
 package de.edirom.meigarage.verovio;
 
+import pl.psnc.dl.ege.configuration.EGEConfigurationManager;
 import pl.psnc.dl.ege.exception.ConverterException;
 import pl.psnc.dl.ege.types.ConversionActionArguments;
 import pl.psnc.dl.ege.types.DataType;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
@@ -33,8 +36,14 @@ public class VerovioConverterTest {
         String tempDir = "src/test/temp";
         converter.convert(is, os, conversionActionArguments, tempDir);
         assertNotNull(new File("src/test/resources/output.svg.zip"));
+        InputStream isout = new FileInputStream("src/test/resources/output.svg.zip");
+        EGEConfigurationManager.getInstance().getStandardIOResolver().decompressStream(isout, new File("src/test/resources/output.svg"));
+        assertEquals("Files differ",
+                new String(Files.readAllBytes(Paths.get("src/test/resources/expected-output.svg"))).replaceAll("id=\"[\\s\\S]*?\"|xlink:href=\"[\\s\\S]*?\"|class=\"[\\s\\S]*?\"",""),
+                new String(Files.readAllBytes(Paths.get("src/test/resources/output.svg/output_001.svg"))).replaceAll("id=\"[\\s\\S]*?\"|xlink:href=\"[\\s\\S]*?\"|class=\"[\\s\\S]*?\"",""));
         is.close();
         os.close();
+        isout.close();
     }
 
     @org.junit.Test
